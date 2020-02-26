a786a0c07967d09e121cbfd40be8dbc67458b868
oleg-serf <olegserfcompany@gmail.com>
2020-02-26 16:12:19 +0200

    fix: goals on user profile

--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -3,7 +3,7 @@
   <div class="user-profile container">
     <div class="top-bar">
       <div class="user__avatar">
-        <img v-if="avatar !== 'null'" :src="$settings.images_path.users + 's_'+ user.avatar" />
+        <img v-if="avatar !== 'null'" :src="$settings.images_path.users + 'm_'+ user.avatar" />
         <span v-else>{{ user.full_name | filterAvatar}}</span>
       </div>
       <div class="user__name">{{user.full_name}}</div>
@@ -28,9 +28,9 @@
         <div class="column">
           <h2>Goals</h2>
           <ul class="goals-list">
-            <!-- <li v-for="goal in goals" :key="goal.id">
+            <li v-for="goal in goals" :key="goal.id">
               <img :src="$settings.images_path.goals + 'm_' + goal.image" alt="icon" />
-            </li>-->
+            </li>
           </ul>
         </div>
         <div class="column">
@@ -96,25 +96,14 @@ export default {
   },
   mounted() {
     axios
-      .get("/goals")
-      .then(res => {
-        // console.log(res.data.goal_categories[0].goals);
-        // Random goals
-        let resultGoals = res.data.goal_categories[0].goals;
-        let goalsCount = Math.floor(Math.random() * Math.floor(16));
-
-        for (let index = 0; index <= goalsCount; index++) {
-          let goalIndex = Math.floor(Math.random() * Math.floor(16));
-          this.goals.push(resultGoals[goalIndex]);
-        }
-      })
-      .catch(error => console.error(error));
-
-    axios
       .get("/users/settings")
       .then(response => {
-        console.log(response.data.user);
+        console.log(response.data);
         this.user = response.data.user;
+        this.goals = response.data.all_goals.filter(item =>
+          response.data.goals.includes(item.id)
+        );
+        console.log(this.goals);
       })
       .catch(error => console.error(error.request));
   }
