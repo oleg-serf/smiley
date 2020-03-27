ef1db13b5702984c657e4eeb713ceade303ca3c4
oleg-serf <olegserfcompany@gmail.com>
2020-03-27 15:46:35 +0200

    scoped news css

--- src/views/News.vue
+++ src/views/News.vue
@@ -258,7 +258,7 @@ export default {
 </script>
 
 
-<style lang="scss">
+<style lang="scss" scoped>
 .main-title.smiley-news-center h2 {
   display: flex !important;
   justify-content: space-between;
