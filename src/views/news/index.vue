9932567fd01f72a7059542f6247a4d1cf6a597f0
oleg-serf <olegserfcompany@gmail.com>
2020-07-21 16:05:33 +0300

    Added route index for news

---
+++ src/views/news/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+    <Breadcrumbs/>
+    <router-view key="$route.fullPath"></router-view>
+    <Footer/>
+
+  </div>
+</template>
+
+<script>
+    import Breadcrumbs from "@/components/Breadcrumbs";
+    import Footer from "@/components/Footer";
+
+    export default {
+        components: {
+            Footer,
+            Breadcrumbs
+        },
+        mounted() {
+            console.log('Router', this.$route);
+        }
+    }
+</script>
