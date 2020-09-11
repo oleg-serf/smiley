0936b6ccc3e4a1bdc91c1cc79676c49f66842392
oleg-serf <olegserfcompany@gmail.com>
2020-09-11 20:38:20 +0300

    Fixed connect button display

--- src/components/feed/UsersList.vue
+++ src/components/feed/UsersList.vue
@@ -269,8 +269,7 @@ export default {
   }
 
   .user-list__user-connect__actions {
-    display: inline-block;
-    max-width: 130px;
+    max-width: 200px;
     text-align: center;
     margin-top: 6px;
     color: #fff;
@@ -278,7 +277,6 @@ export default {
     text-decoration: none;
     font-size: 0.8rem;
     font-family: "Montserrat SemiBold", sans-serif;
-    display: block;
     padding: 2px 15px;
     text-transform: uppercase;
     height: auto;
