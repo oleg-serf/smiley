11ab06f46684f090faee1217dabf9cc382be0653
oleg-serf <olegserfcompany@gmail.com>
2020-09-28 12:33:03 +0300

    Test

--- src/views/test-youtube.vue
+++ src/views/test-youtube.vue
@@ -5,6 +5,7 @@
         <span>Put your YouTube Video ID here:</span> <br>
       <input type="text" v-model="video_id">
       </label>
+
       <br>
       <div style="height: 0px; padding-bottom: 56.25%; width: 100%; position: relative">
       <iframe style="width: 100%; height: 100%; position: absolute; left: 0px; top: 0px; object-fit: cover;" :src="'https://www.youtube.com/embed/'+video_id" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
