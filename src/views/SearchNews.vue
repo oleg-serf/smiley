158796129334c635be16080c6af57858f79326b9
oleg-serf <olegserfcompany@gmail.com>
2020-12-28 23:25:16 +0200

    News search fix

--- src/views/SearchNews.vue
+++ src/views/SearchNews.vue
@@ -104,7 +104,7 @@ export default {
       axios
         // .get("/search?news-page=" + pageNum)
         .get(
-          "/news-search?news-page=" +
+          "/api/news?news-page=" +
             pageNum +
             "&keyword=" +
             this.$route.params.keyword
@@ -134,7 +134,7 @@ export default {
   },
   mounted() {
     axios
-      .get("/news-search?keyword=" + this.$route.params.keyword)
+      .get("/api/news?keyword=" + this.$route.params.keyword)
       .then(res => {
         console.log("Search results loaded", res);
 
